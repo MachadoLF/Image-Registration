c3a79be36903dce803605836c0f7a680065a2eb7
Leonardo Machado <leodemachado@gmail.com>
2020-03-27 12:49:57 -0400

    3D resgitrations with non-normalized Tsallis Entropy.

---
+++ NonNormalizedTestedAlgorithms/3DMachadoMIRunner/debug/CMakeFiles/3DMachadoMIRunner.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "3DMachadoMIRunner"
+  "3DMachadoMIRunner.pdb"
+  "CMakeFiles/3DMachadoMIRunner.dir/3DMachadoMIRunner.cxx.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3DMachadoMIRunner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
