c46faad2f16330fbec103703dd9ea55d49972a17
Leonardo Machado <leodemachado@gmail.com>
2020-03-27 12:55:36 -0400

    Moving folders.

---
+++ NonNormalizedTestedAlgorithms/2DMachadoMIOptimizer/build/CMakeFiles/MachadoMIOptimizer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MachadoMIOptimizer.dir/MachadoMIOptimizer.cxx.o"
+  "MachadoMIOptimizer"
+  "MachadoMIOptimizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MachadoMIOptimizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ NonNormalizedTestedAlgorithms/2DMachadoMIOptimizer/build/CMakeFiles/MachadoMIOptimizer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MachadoMIOptimizer.
+# This may be replaced when dependencies are built.
