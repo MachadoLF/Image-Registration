db839a2eb1105565d4e15eae3cacb902ed498318
Leonardo Machado <leodemachado@gmail.com>
2020-03-31 19:22:03 -0400

    Itk Example with modifications.

---
+++ GeneralTestsAndExamples/MultiStage_raw/debug/CMakeFiles/MultiStageImageRegistration1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MultiStageImageRegistration1.dir/MultiStageImageRegistration1.cxx.o"
+  "MultiStageImageRegistration1"
+  "MultiStageImageRegistration1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MultiStageImageRegistration1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
