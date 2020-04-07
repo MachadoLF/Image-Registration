bc40d06e102d9e0964d9d5664d74903dc0eeb306
Leonardo Machado <leodemachado@gmail.com>
2020-04-07 17:29:54 -0400

    checked divergences on 3D implementations.

---
+++ NonNormalizedTestedAlgorithms/3DMachadoMIOptimizer/debug/CMakeFiles/MachadoMIOptimizer3D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MachadoMIOptimizer3D.dir/MachadoMIOptimizer3D.cxx.o"
+  "MachadoMIOptimizer3D"
+  "MachadoMIOptimizer3D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MachadoMIOptimizer3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- NonNormalizedTestedAlgorithms/3DMachadoMIOptimizer/debug/cmake_install.cmake
+++ NonNormalizedTestedAlgorithms/3DMachadoMIOptimizer/debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /PHShome/lfm23/Desktop/Doutorado/MODULES/3DMachadoMIOptimizer
+# Install script for directory: /home/leo/Desktop/REPOSITORIES/Image-Registration/NonNormalizedTestedAlgorithms/3DMachadoMIOptimizer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/PHShome/lfm23/Desktop/Doutorado/MODULES/3DMachadoMIOptimizer/debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/leo/Desktop/REPOSITORIES/Image-Registration/NonNormalizedTestedAlgorithms/3DMachadoMIOptimizer/debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
