eb732e27869f8c33920bd38f9e0041436ca56057
Leo <leonardomachado@usp.br>
2020-10-05 10:16:31 -0300

    Deformable-first version.

---
+++ NonNormalizedAlgorithms/MachadoMIMultiStageDeformable/debug/CMakeFiles/MachadoMIMultiStageDeformable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MachadoMIMultiStageDeformable.dir/MachadoMIMultiStageDeformable.cxx.o"
+  "MachadoMIMultiStageDeformable.pdb"
+  "MachadoMIMultiStageDeformable"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MachadoMIMultiStageDeformable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
